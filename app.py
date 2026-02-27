a8d565c1e45b714f23b17e9d8f1f1a6a9dfebf32
Trupti Chavan <truptimchavan@gmail.com>
2026-02-27 22:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import pickle
-from transformers import pipeline
 from langchain_community.llms import HuggingFacePipeline
 
 # -----------------------------
